3a97f9e225e9db877ba3734b825c1e4d6bc1fa2a
YasirAli851 <yasir128.ali@gmail.com>
2025-11-09 17:24:36 +0530

    made changes on base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/medicalApp/",
+//   plugins: [react()],
+// });
+// vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// The base path MUST match your GitHub repository name exactly.
 export default defineConfig({
-  base: "/medicalApp/",
+  base: "/medical-registry/", // <--- CHANGED FROM /medicalApp/
   plugins: [react()],
 });
